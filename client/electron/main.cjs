ebe5029055ff948d44926e1b9ad33359ba1fbf74
Miftahul Islam Tashfin <miftahul.islam.tashfin@gmail.com>
2026-06-04 04:19:28 +0600

    Add Capacitor Android project and Electron Desktop wrapper

---
+++ client/electron/main.cjs
@@ -0,0 +1,36 @@
+const { app, BrowserWindow } = require('electron');
+const path = require('path');
+const isDev = process.env.NODE_ENV !== 'production';
+
+function createWindow() {
+  const mainWindow = new BrowserWindow({
+    width: 1280,
+    height: 800,
+    webPreferences: {
+      nodeIntegration: true,
+      contextIsolation: false
+    }
+  });
+
+  if (isDev) {
+    // In development, load the Vite dev server
+    mainWindow.loadURL('http://localhost:5173');
+    // Open the DevTools.
+    mainWindow.webContents.openDevTools();
+  } else {
+    // In production, load the built index.html
+    mainWindow.loadFile(path.join(__dirname, '../dist/index.html'));
+  }
+}
+
+app.whenReady().then(() => {
+  createWindow();
+
+  app.on('activate', function () {
+    if (BrowserWindow.getAllWindows().length === 0) createWindow();
+  });
+});
+
+app.on('window-all-closed', function () {
+  if (process.platform !== 'darwin') app.quit();
+});
